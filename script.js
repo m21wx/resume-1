907a1a9c1fd4f0ba8eee89af1b5f7c69fbf59c8f
m21wx <126360340+m21wx@users.noreply.github.com>
2024-07-26 23:40:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-	
 		let content = document.querySelector('.resume-1 .contact')
 		let btnIcons = document.querySelector('.resume-1 .contact .icons span');
-		console.log(btnIcons)
+		btnIcons.innerHTML='<i class="fa-solid fa-chevron-left"></i>';
 		
 		btnIcons.addEventListener('click',()=>{
 			content.classList.toggle('active')
+			if(content.classList.contains('active')){
+				btnIcons.innerHTML=`<i class="fa-solid fa-chevron-right"></i>`;
+			}else{
+				btnIcons.innerHTML=`<i class="fa-solid fa-chevron-left"></i>`;
+			}
 		})
